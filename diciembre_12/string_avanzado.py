1df0740dbc60bd7923cd952f7d4a20d6e2b80d15
Patricio <patricio.fuentealba.feliu@gmail.com>
2025-12-12 19:47:18 -0300

    Funciones avanzadas de texto parte 1

---
+++ diciembre_12/string_avanzado.py
@@ -0,0 +1,43 @@
+# Manipulación avanzada de strings
+# strip () , lstrip(), rstrip()
+
+texto="       Python avanzado     "
+print(f"|{texto}|")
+print(f"|{texto.strip()}|") # elimina los espacios a la derecha e izquierda
+print(f"|{texto.lstrip()}|") # elimina los espacios a la izquierda
+print(f"|{texto.rstrip()}|") # elimina los espacios a la derecha
+
+# lower() , upper() , capitalize() , title()
+nombre="franCiscO"
+titulo="hola francisco gracias por venir"
+print(f"{nombre.lower()}")  # convierte todo a minúscula
+print(f"{nombre.upper()}") # convierte todo a mayúscula
+print(f"{titulo.capitalize()}") # deja solo la letra inicial en mayúscula
+print(f"{titulo.title()}") # cambia a mayúscula la primera letra de cada palabra
+
+# replace()
+mensaje="Hola mundo"
+nuevo_mensaje=mensaje.replace("o","0") #busca el primer argumento y lo reemplza por el segundo
+print(nuevo_mensaje)
+
+# startwith(), endswidth()  , entrega True o False
+archivo="documento.pdf"
+print(archivo.endswith(".pdf"))
+print(archivo.startswith("doc"))
+
+# join() 
+palabras=["Hola","a","todos"]
+frase=" ".join(palabras)
+print(frase)
+
+# split()  nos entrega una lista con los elementos separados
+linea="nombre,apellido,edad,direccion,email"
+cabecera=linea.split(",") # por defecto el caracter divisor es el espacio
+print(cabecera)
+
+texto="   Hola, me llamo Aureliano y me gusta Python!!    "
+# 1.- Quitar espacios sobrantes al principio y al final
+# 2.- Convertir todo a minúsculas
+# 3.- Reemplazar Aureliano por su nombre
+# 4.- Verificar si el texto termina con "Python!!"
+# 5.- Dividir el texto en palabras y mostar cuantas tiene
