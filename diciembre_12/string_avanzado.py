0a65dd74707f16b63fe0d6be5827c836c3675fe2
Patricio <patricio.fuentealba.feliu@gmail.com>
2025-12-12 20:00:52 -0300

    Funciones avanzadas de texto parte 2

--- diciembre_12/string_avanzado.py
+++ diciembre_12/string_avanzado.py
@@ -7,15 +7,16 @@ print(f"|{texto.strip()}|") # elimina los espacios a la derecha e izquierda
 print(f"|{texto.lstrip()}|") # elimina los espacios a la izquierda
 print(f"|{texto.rstrip()}|") # elimina los espacios a la derecha
 
-# lower() , upper() , capitalize() , title()
+# lower() , upper() , capitalize() , title() , swapcase()
 nombre="franCiscO"
 titulo="hola francisco gracias por venir"
 print(f"{nombre.lower()}")  # convierte todo a minúscula
 print(f"{nombre.upper()}") # convierte todo a mayúscula
 print(f"{titulo.capitalize()}") # deja solo la letra inicial en mayúscula
 print(f"{titulo.title()}") # cambia a mayúscula la primera letra de cada palabra
+print(f"{titulo.swapcase()}") # intercambia mayúsculas por minúsculas y viceversa.
 
-# replace()
+# replace(texto_que_busca,texto_que_reemplaza)
 mensaje="Hola mundo"
 nuevo_mensaje=mensaje.replace("o","0") #busca el primer argumento y lo reemplza por el segundo
 print(nuevo_mensaje)
@@ -37,7 +38,15 @@ print(cabecera)
 
 texto="   Hola, me llamo Aureliano y me gusta Python!!    "
 # 1.- Quitar espacios sobrantes al principio y al final
+texto = texto.strip()
+print(texto+"|")
 # 2.- Convertir todo a minúsculas
+print(f"{texto.lower()}")
 # 3.- Reemplazar Aureliano por su nombre
+
 # 4.- Verificar si el texto termina con "Python!!"
+
 # 5.- Dividir el texto en palabras y mostar cuantas tiene
+palabras = texto.split()
+print(palabras)
+print(len(palabras))
