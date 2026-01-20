00aab3bde5dc0eff2b4f8ced66f421bfb384a774
Patricio <patricio.fuentealba.feliu@gmail.com>
2026-01-20 20:15:22 -0300

    escritura de archivos parte 1

---
+++ enero_20/ejemplo_01/escribir.py
@@ -0,0 +1,18 @@
+import os
+BASE_DIR=os.path.dirname(__file__)
+nombre_archivo="salida.txt"
+RUTA_ARCHIVO=os.path.join(BASE_DIR,nombre_archivo)
+
+with open(RUTA_ARCHIVO,"w",encoding="utf-8") as archivo:
+    archivo.write("Hola mundo 2\n")
+    archivo.write("cómo están?\n")
+
+    while True:
+        mensaje=input("Ingrese su mensaje o vacio para salir:")
+        if mensaje =="":
+            break
+        archivo.write(mensaje)
+        archivo.write("\n")
+    
+    varias_lineas=['primera linea\n','segunda linea\n','tercera linea\n']
+    archivo.writelines(varias_lineas)
