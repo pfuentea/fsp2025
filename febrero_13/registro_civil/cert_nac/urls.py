80c22659530dea6ffac1c3f94e43a020b998595f
Patricio <patricio.fuentealba.feliu@gmail.com>
2026-02-16 22:18:04 -0300

    uso de session y bootstrap en plantillas

--- febrero_13/registro_civil/cert_nac/urls.py
+++ febrero_13/registro_civil/cert_nac/urls.py
@@ -4,7 +4,10 @@ from . import views
 # LISTADO DE TRAMITES QUE PODREMOS REALiZAR
 urlpatterns =[
     path('',views.index, name='index'), 
+    path('<str:usuario>',views.index, name='index'), 
+    path('<str:usuario>/<int:veces>',views.index, name='index'), 
     path('servicios/',views.servicios, name='servicios'), 
     path('contacto/',views.contacto, name='contacto'), 
     path('nosotros/',views.nosotros, name='nosotros'), 
+
 ] 
--- febrero_13/registro_civil/cert_nac/views.py
+++ febrero_13/registro_civil/cert_nac/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 # Create your views here.
 
-def index(request):
+def index(request,usuario="",veces=1):
     #Acá va la lógica del negocio
     # render es un método que nos sirve para RENDERIZAR una
     # plantilla HTML
 
+    # voy a crear una variable de sesion para guardar al usuario
+    
+
     #creamos una variable de contexto, que es un diccionario
-    usuario='Gloria'
+    if usuario != "":
+        usuario=usuario
+    else:
+        usuario='Visitante'
+
+    request.session['username']=usuario
+
+
     lugar='Santiago'
+
+    repeticiones=[v for v in range(1,veces+1)]
 
     title=f'Reg Civ de {lugar}'
 
@@ -16,18 +28,26 @@ def index(request):
         'nombre':usuario,
         'ciudad':lugar,
         'title':title,
+        'opcion':2,
+        'veces':repeticiones,
     }
     return render(request,'index.html',context=context )
 
 def servicios(request):
+
+
     context={}
     return render(request,'servicios.html',context=context )
-
 
 def nosotros(request):
     context={}
     return render(request,'nosotros.html',context=context )
 
 def contacto(request):
-    context={}
+    
+    usuario=request.session['username']
+
+    context={
+        'usuario':usuario,
+    }
     return render(request,'contacto.html',context=context )
