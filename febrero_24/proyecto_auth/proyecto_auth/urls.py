a3d946fab674ab17f347fd7e339d49c6a357ea15
Patricio <patricio.fuentealba.feliu@gmail.com>
2026-02-24 21:10:43 -0300

    enunciado del ejercicio

--- febrero_24/proyecto_auth/proyecto_auth/urls.py
+++ febrero_24/proyecto_auth/proyecto_auth/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', LoginView.as_view(template_name='login.html'),name='login' ),
     path('login/', LoginView.as_view(template_name='login.html'),name='login' ),
     path('logout/', LogoutView.as_view(),name='logout' ),
-    path('',include('auth_app.urls'))
+    path('',include('auth_app.urls')),
 ]
