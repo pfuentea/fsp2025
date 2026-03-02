5903dec151d5af3f93bf8ade23697ab56c793ff7
Patricio <patricio.fuentealba.feliu@gmail.com>
2026-03-02 20:31:13 -0300

    enunciado

--- marzo_02/retaurant/comida_app/urls.py
+++ marzo_02/retaurant/comida_app/urls.py
@@ -8,6 +8,21 @@ urlpatterns = [
     path('ingredientes/', views.IngredientesListView.as_view(),name='ingredientes_list'), 
     path('ingredientes/nuevo', views.IngredientesCreateView.as_view(),name='ingredientes_create'),
     # falta edita, detalle, borrar
+'''
+    # CRUD Sandwich + ingredientes
+    path('sandwiches/', views.SandwichesListView.as_view(),name='sandwiches_list'),
+    path('sandwiches/nuevo', views.SandwichesCreateView.as_view(),name='sandwiches_create'),
+# falta edita, detalle, borrar
 
+    #Pedidos
+    path('pedidos/nuevo', views.PedidosCreateView.as_view(),name='pedido_create'),
+    path('pedidos/<int:pk>', views.PedidosDetailView.as_view(),name='pedido_detalle'),
+    # falta edita, detalle, borrar
+
+    #Detalle pedido
+    path('pedidos/<int:orde_id>/items/nuevo', views.PedidoDetalleCreateView.as_view(),name='pedido_detalle_create'),
+    # falta edita, detalle, borrar
+
+'''
 
 ]
