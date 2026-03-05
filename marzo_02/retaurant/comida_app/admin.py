ad706879d88a7d4616c0290307299580f224d65b
Patricio <patricio.fuentealba.feliu@gmail.com>
2026-03-04 21:18:04 -0300

    checklist y upload de imagenes en admin

--- marzo_02/retaurant/comida_app/admin.py
+++ marzo_02/retaurant/comida_app/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
 from .models import Ingrediente, Sandwich,Pedido,DetallePedido
+from django.utils.html import format_html
 # Register your models here.
 
 #admin.site.register(Ingrediente)
 @admin.register(Ingrediente)
 class IngredienteAdmin(admin.ModelAdmin):
-    list_display=('nombre','categoria','precio')
+    list_display=('nombre','categoria','precio','imagen_preview')
     list_filter=('categoria',)
     search_fields=('nombre','categoria',) # busca en el nombre y la categoria
     ordering = ('nombre',)
     list_per_page= 3
+
+    def imagen_preview(self,obj):
+        if obj.imagen:
+            return format_html('<img src="{}" width=50 />',obj.imagen.url)
+        return "-"
+    imagen_preview.short_description ="Imagen"
+
 
 #admin.site.register(Sandwich)
 @admin.register(Sandwich)
--- marzo_02/retaurant/comida_app/models.py
+++ marzo_02/retaurant/comida_app/models.py
@@ -19,6 +19,8 @@ class Ingrediente(models.Model):
     nombre= models.CharField(max_length=50,unique=True)
     precio=models.DecimalField(max_digits=10,decimal_places=2,default=0)
     categoria=models.CharField(max_length=50,choices=Category.choices)
+    
+    imagen = models.ImageField(upload_to='ingredientes/',null=True,blank=True)
     # sandwich_de_ingrediente
     # extras_en_pedidos
 
