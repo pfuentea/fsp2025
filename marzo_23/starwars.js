6c6e6ba7eaf23277debf14b2fd491a7886136e99
Patricio <patricio.fuentealba.feliu@gmail.com>
2026-03-23 19:57:37 -0300

    inicio proyecto api sw v1.1

--- marzo_23/starwars.js
+++ marzo_23/starwars.js
@@ -5,15 +5,17 @@ $(document).ready(function(){
     })
 
     function cargar(){
+        //definimos la url de la api (endpoint)
         let url_base='https://akabab.github.io/starwars-api/api/all.json';
-    
-        $.get(url_base,function(data){
-    
+        // hacemos una llamada a la API
+        $.get(url_base,function(data){ // en data queda la respuesta
+            // tomamos los primeros 10 elemento 0-9 y los dejamos en el arreglo personajes
             let personajes = data.slice(0,9);
-    
+            // recorremos el arreglo y vamos tomando los campos que vamos a usar
             personajes.forEach(p => {
-                console.log(p);
-    
+                
+                console.log(p);// mostrando la info por cada elemento del arreglo
+                //construimos la visualización de la data
                 let carta=`
                 <div>
                     <img src="${p.image}" width="200px" heigth="250px" >
@@ -22,6 +24,7 @@ $(document).ready(function(){
                     <p>${p.species}-${p.homeworld} </p>
                 </div>     
                 `;
+                // lo agregamos al DOM al final de todo
                 $("body").append(carta);
     
             } );
