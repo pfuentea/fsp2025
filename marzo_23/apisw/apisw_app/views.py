31e91c4faad484012c59b76244718e004973a2fb
Patricio <patricio.fuentealba.feliu@gmail.com>
2026-03-23 22:03:01 -0300

    API con DJANGO

---
+++ marzo_23/apisw/apisw_app/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+import requests
+from django.http import JsonResponse
+# Create your views here.
+
+API_KEY=''
+
+def dashboard(request):
+
+    url_base='http://api.weatherapi.com/v1'
+
+    url_base='http://api.weatherapi.com/v1/current.json?q=Valdivia&aqi=no'
+    headers={
+        "Authorization":f"Bearer {API_KEY}"
+    }
+    try:
+        response=requests.get(url_base,timeout=10,headers=headers)
+        data=response.json()
+        #aca se cocina la informacion para pasarla al template
+    except requests.RequestException as e:
+        return JsonResponse({
+            "ok":False,
+            "error":f"Error en la API: {e} "
+        },status=500)
+
+    context={
+        'respuesta':data,
+    }
+    return render(request,'dashboard.html',context=context)
--- marzo_23/starwars.js
+++ marzo_23/starwars.js
@@ -1,12 +1,58 @@
 $(document).ready(function(){
 
+    // ESTA ES LA CARGA INICIAL
+   
+
+    let url_base='https://akabab.github.io/starwars-api/api/all.json';
+    let personajesTotal=[];
+
+    $.get(url_base,function(data){
+        let planetas=[];
+        personajesTotal=data;
+        data.forEach(p => {
+            if(p.homeworld && !planetas.includes(p.homeworld)){
+                planetas.push(p.homeworld);
+            }
+        });
+        planetas.sort();
+
+        planetas.forEach(planeta=>{
+            $('#planetas').append(`<option value="${planeta}">${planeta} </option>`);
+        });
+    });
+
+    $('#planetas').change(function(){
+        let seleccionado=$(this).val();
+        $('#resultado').empty();
+        if (seleccionado==="") return;
+        let filtrados=personajesTotal.filter(p=>
+            p.homeworld===seleccionado
+        );
+
+        $('#titulo').text("Personajes de "+seleccionado);
+
+        filtrados.forEach(p=>{
+            let carta=`
+                <div class="mx-3 mt-3">
+                    <img src="${p.image}" width="200px" height="250px" >
+                    <p></p>
+                    <p5>${p.name} </p5>
+                    <p>${p.species}-${p.homeworld} </p>
+                </div>     
+                `;
+                // lo agregamos al DOM al final de todo
+                $("#resultado").append(carta);
+        });
+
+    });
+
+
     $('#cargar').on('click',function(){
         cargar();
     })
 
     function cargar(){
         //definimos la url de la api (endpoint)
-        let url_base='https://akabab.github.io/starwars-api/api/all.json';
         // hacemos una llamada a la API
         $.get(url_base,function(data){ // en data queda la respuesta
             // tomamos los primeros 10 elemento 0-9 y los dejamos en el arreglo personajes
@@ -25,7 +71,7 @@ $(document).ready(function(){
                 </div>     
                 `;
                 // lo agregamos al DOM al final de todo
-                $("body").append(carta);
+                $("#resultado").append(carta);
     
             } );
         });
